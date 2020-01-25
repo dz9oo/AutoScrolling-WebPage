7a713cd45819d93724063361bbd36936b0b0bc12
Nicolas Collier <nicolascollier@protonmail.ch>
2020-01-25 22:38:45 +0100

    Add automatic scroll when you hover over an item at the edge of the viewport.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,41 @@
 
-// Navbar mune burger:
-function openBurger(elem) {
-    elem.classList.toggle("navbar-burger-change");
-
+// Navbar menu burger:
+document.getElementById("navbar-burger").addEventListener("click", (e) => {
+    e.target.classList.toggle("navbar-burger-change");
     let links = document.getElementById("navbar-links");
-    console.log(links);
     links.classList.toggle("navbar-links-show");
     links.classList.toggle("navbar-links");
+}, true);
 
-}
+
+
+// Automatic scrolling management when you hover over an item at the edge of the viewport.
+let articles = document.getElementsByClassName("article");
+for (article of articles) {
+    article.addEventListener("mouseover", (elem) => {
+        let yPos = 0;
+        let rect = elem.target.getBoundingClientRect();
+
+        yPos = rect.y;
+
+        // Si mouseover au dessus d'un article qui est trop haut sur le viewport
+        if (yPos < 230) {
+            window.scrollBy({
+                top: -100, // could be negative value
+                left: 0,
+                behavior: 'smooth'
+            });
+        } else if (yPos > 410) {
+            window.scrollBy({
+                top: 100, // could be negative value
+                left: 0,
+                behavior: 'smooth'
+            });
+        }
+
+        console.info(`position y : ${yPos}`);
+    }, true);
+};
+
+
+
